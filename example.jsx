3080674c7f005b3a18cbd7315d12ce30b826ddde
Geoff Greer <geoff@greer.fm>
2019-02-01 14:44:57 -0800

    More work.

--- example.jsx
+++ example.jsx
@@ -56,8 +56,8 @@ class Refs extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
-      oValue: 'oValue',
-      value: "value",
+      oValue: '1',
+      value: 'a',
     };
     this.setOValue = e => this.setState({ oValue: e.target.value });
     this.setValue = e => this.setState({ value: e.target.value });
@@ -71,22 +71,26 @@ class Refs extends React.Component {
     return <form className="form">
       <Title id="refs">Races and Refs</Title>
       <p>
-        In React, user defined <code>Components</code> are instantiated by the framework to create a virtual DOM.
-        A <code>ref</code> provides an escape valve to access the realized DOM counterpart of a given component.
-        <br/>
-        <br/>
-        Here, we control the input directly and ask the wrapper what it thinks the value is by way of a <code>ref</code>.
-        The ref&rsquo;s <code>value</code> attribute and our <code>value</code> from our internal state race because of the two way data binding of <code>value</code>.
+        React <code>Components</code> are instantiated by the framework to create a virtual DOM.
+        React&apos;s <a href="https://reactjs.org/docs/refs-and-the-dom.html"><code>ref</code> API</a> provides a way to access the realized DOM counterpart of a given component.
       </p>
       <p>
-        It is probably possible to fix this bug in the wrapper, but it was the 3rd or 4th race we ran into.
+        Here, we control the input directly and ask the wrapper what it thinks the value is by way of a <code>ref</code>.
+        The ref&apos;s <code>value</code> attribute and our <code>value</code> sometimes differ because of the two way data binding.
       </p>
-      <FieldSet label="Controlled Input:" aside="Can be changed by user. Always up to date.">
+      <p>
+        It is probably possible to fix this bug in the wrapper, but it was the 3rd or 4th race condition we ran into after a few hours of combined effort.
+        The React equivalent of <code>o-input</code> took us about a minute to write.
+      </p>
+      <FieldSet label="Native Input:" aside="Can be changed by user. Always up to date.">
         <input className="text-input" name="native" type="text" value={value} onChange={this.setValue} ref={this.ref} />
-        ref value: { this.ref.current && this.ref.current.value }
       </FieldSet>
-      <OTextInput label="Web component" name="wc1" value={oValue} onChange={this.setOValue} ref={this.oRef} />
-      ref value: { this.oRef.current && this.oRef.current.value }
+      value from input ref: { this.ref.current && this.ref.current.value }
+      <br/>
+      <br/>
+      <OTextInput label="o-input wrapper" name="wc1" value={oValue} onChange={this.setOValue} ref={this.oRef} />
+      <br/>
+      value from o-input ref: { this.oRef.current && this.oRef.current.value }
     </form>;
   }
 }
@@ -96,7 +100,7 @@ class ControlledInput extends React.Component {
     super(props);
     this.state = {
       eventKeys: [''],
-      value: "test",
+      value: "value",
     };
     this.setValue = e => {
       this.setState({
@@ -109,27 +113,31 @@ class ControlledInput extends React.Component {
   render () {
     const { eventKeys, value } = this.state;
     return <form className="form">
-      <Title id="custom-events">Custom Events</Title>
+      <Title id="events">Events</Title>
       <p>
-        In React, <code>value=</code> creates a <a href="https://reactjs.org/docs/forms.html#controlled-components">controlled input</a> - the value is entirely controlled by the Component that renders it.
-        React informs us when a controlled input changes by way of the <a href="https://reactjs.org/docs/handling-events.html"><i>changed</i> event</a>:
+        Most events bubble up from the actual input because our Web Components aren&apos;t using the shadow DOM.
+        One exception is <code>o-input</code>&apos;s <code>oChange</code> event, which is emitted instead of the native <code>input</code> event.
       </p>
 
+      From React&apos;s <a href="https://reactjs.org/docs/events.html">SyntheticEvent docs</a>:
       <blockquote>
         Your event handlers will be passed instances of SyntheticEvent, a cross-browser wrapper around the browser’s native event.
-        It has the same interface as the browser&#39;s native event, including stopPropagation() and preventDefault(), except the events work identically across all browsers.
+        It has the same interface as the browser&apos;s native event, including <code>stopPropagation()</code> and <code>preventDefault()</code>, except the events work identically across all browsers.
         ...React normalizes events so that they have consistent properties across different browsers.
       </blockquote>
 
       <p>
-        The React o-input wrapper must manually <code>(addEventListener)</code> bind to the o-change custom event because React does not listen to custom events
-        which bypasses React&#39;s event normilization.
-        Options are:
+        The React <code>o-input</code> wrapper must manually bind to <code>oChange</code> using <code>addEventListener()</code>.
+        This bypasses React&apos;s event normalization.
+      </p>
+      <p>
+        Solutions include:
       </p>
       <ol>
-        <li>abandon event compatibility with React</li>
-        <li>do not use custom events</li>
-        <li>duplicate reacts event normilization</li>
+        <li>Abandon event compatibility with React.</li>
+        <li>Do not use custom events in Web Components.</li>
+        <li>Duplicate React&apos;s event normalization.</li>
+        <li>Do not use Web Components in React.</li>
       </ol>
 
       <FieldSet label="Controlled Input:">
@@ -137,14 +145,15 @@ class ControlledInput extends React.Component {
       </FieldSet>
       <OTextInput label="(Controlled) o-input" name="wc1" value={value} onChange={this.setValue} />
       <p>
-        Keys from change event object:
+        Keys from change event:
         { eventKeys && <br/> }
         { eventKeys.map(k => <React.Fragment key={k}>{k}<br/></React.Fragment>)}
       </p>
     </form>;
   }
 }
-
+//         In React, passing a <code>value=</code> property to an input creates a <a href="https://reactjs.org/docs/forms.html#controlled-components">controlled input</a>. The value is entirely controlled by the Component that renders it.
+//         React informs us when a controlled input changes by way of the <a href="https://reactjs.org/docs/handling-events.html"><code>onChange</code> event handler</a>:
 class Events extends React.Component {
   constructor (props) {
     super(props);
@@ -155,11 +164,10 @@ class Events extends React.Component {
 
   render () {
     return <form className="form">
-      <Title>Events</Title>
+      <Title id="interfaces">Interfaces</Title>
       <p>
-        Most events bubble up from the actual input because we aren&rsquo;t using the shadow dom.
-        However, the <code>onChange</code> handler in the wrapper must be bound to the <code>oChange</code> custom event which targets the o-input, (not the actual input).
-        o-input is currently missing most <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement#properties">methods and properties</a> that inputs have:
+        Custom events emitted by Web Components change the event&apos;s target from the actual emitter (<code>input</code>) to the web component (<code>o-input</code>).
+        &nbsp;<code>o-input</code> is currently missing most of the native input&apos;s <a href="https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement#properties">methods and properties</a>:
       </p>
       <code>
         blur()
@@ -172,11 +180,8 @@ class Events extends React.Component {
         checkValidity()
         reportValidity()
       </code>
-      <p>
-        Of the ones that do mysteriously work, its not at all clear why since operating on o-input is not the same as operating on input.
-        Besides the ones listed above, there are input specific methods and several vendor specific methods/properties potentially used by libraries.
-        The list of <i>common</i> properties that must be supported on both sides is large:
-      </p>
+      <br/>
+      <br/>
       <code>
         name,
         type,
@@ -201,8 +206,11 @@ class Events extends React.Component {
         size,
         tabindex
       </code>
-      <br/>
-      <br/>
+      <p>
+        Besides the ones listed above, there are input-specific methods and several vendor-specific methods/properties potentially used by libraries.
+        These missing methods and properties also break the expected interface of <code>inputs</code> accessed via <code>ref</code>s.
+      </p>
+      <br />
       <FieldSet
         label="Native Input:"
         aside={<button type="button" className="button is-button-primary" onClick={() => this.input.focus()}>focus input</button>}>
@@ -217,7 +225,7 @@ class Events extends React.Component {
         ref={r => this.oInput = r}
         onClick={e => console.log('WC onClick', e.target)}
         onChange={e => console.log('onChange', e.target)}
-        label="Web Component has no `focus` method"
+        label="Web component has no `focus` method"
       >
         <button type="button" onClick={() => this.oInput.focus()}>focus WC</button>
       </OTextInput>
@@ -228,9 +236,9 @@ class Events extends React.Component {
 const Styles = () => <form className="form">
   <Title id="styles">Idiomatic Code, Composition, and Customization</Title>
   <p>
-    Web Components may only accept string attributes (props) because thats the limition of the DOM.
-    React Components can receive any type of data because they only exist in React&rsquo;s virtual DOM.
-    In this case, the <code>label</code> is actually the short snippet:
+    Web Components may only accept attributes that are strings.
+    React Components can receive any type of data.
+    In this case, the input&apos;s <code>label</code> is actually a short snippet:
   </p>
   <pre>{`const label = <code>
   <img src="fa-triangle.png" style={{ width: 18, height: 18 }} /> 
@@ -239,8 +247,8 @@ const Styles = () => <form className="form">
 ...
 <FieldSet label={label} ... />`}</pre>
   <p>
-    This is a common pattern in React, which strongly prefers Composition over other abstractions.
-    Components themselves can (and commonly are) passed as arguments to other Components like so:
+    This is a common pattern in React, which strongly prefers composition over other abstractions.
+    Components themselves can be (and commonly are) passed as arguments to other Components, like so:
   </p>
 
   <pre>{`const CustomLabel = (props) => <code style={props.style}>
@@ -250,85 +258,37 @@ const Styles = () => <form className="form">
 ...
 <FieldSet label={CustomLabel} ... />`}</pre>
   <p>
-    This sort of idiomatic interface (Higher Order Components et al) is not even possible with slots, because Web Components can not operate on React Components, only the DOM nodes they render.
-    Even something trivial in React like passing a list of strings to a component becomes hard.
+    This sort of idiomatic interface (Higher Order Components et al) is not possible with slots. Web Components cannot operate on React Components, only the DOM nodes they render.
+    E.g, Web Components can not accept a <i>p tag</i> in a slot.
+    Even something trivial in React (like passing a list of strings to a component) becomes hard.
   </p>
   <p>
-    Because React <code>style</code> and <code>className</code> attributes are applied directly to the <code>o-input</code>, there is no clean way to customize <code>o-input</code>&rsquo;s <code>label</code>.
-    These sorts of one-off problems inevitably turn into bikesheds and generate Frankenstein interfaces because universal (Web) Components must actually meet the unholy union of all product requirements from all the products that use them.
+    Because React <code>style</code> and <code>className</code> props are applied directly to the <code>o-input</code>, there is no clean way to customize <code>o-input</code>&apos;s <code>label</code>.
+    These sorts of one-off problems inevitably turn into bikesheds and generate Frankenstein interfaces because universal (Web) Components must satisfy all product requirements from all the products that use them.
   </p>
   <FieldSet className="fieldset"
     label={<code><img src="fa-triangle.png" style={{ width: 18, height: 18 }} /> http://</code>}>
     <input className="text-input" type="text" defaultValue="Fancy label" />
   </FieldSet>
-  <OTextInput label={<code><img src="fa-triangle.png" style={{ width: 18, height: 18 }} /> http://</code>} style={{fontFamily: 'Impact'}} className="hello">
-  </OTextInput>
+  <OTextInput label={<code><img src="fa-triangle.png" style={{ width: 18, height: 18 }} /> http://</code>} style={{fontFamily: 'Impact'}} className="hello" />
 </form>;
 
-class List extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      items: [],
-    };
-    this.input = React.createRef();
-    this.add = () => {
-      this.setState({
-        items: [...this.state.items, this.input.current.value],
-      });
-      this.input.current.value = '';
-      this.input.current.focus();
-    };
-    this.remove = i => {
-      let items = [...this.state.items];
-      items.splice(i, 1);
-      this.setState({ items });
-    };
-  }
-
-  render () {
-    const { items } = this.state;
-    return <React.Fragment>
-      <h1>{items.length} Item{items.length === 1 ? '' : 's'}</h1>
-      <input type="text" placeholder="type here" ref={this.input} />
-      <button type="button" onClick={this.add}>Add!</button>
-      <hr />
-      { items.map((item, i) => <FieldSet
-        key={item}
-        label={`Item ${i + 1}`}
-        aside={<button type="button" onClick={() => this.remove(i)}>Remove!</button>}>
-        <input className="text-input" type="text" defaultValue={item} />
-      </FieldSet>) }
-    </React.Fragment>;
-  }
-}
-
-class OList extends List {
-  render () {
-    const { items } = this.state;
-    return <React.Fragment>
-      <h1>WC {items.length} Item{items.length === 1 ? '' : 's'}</h1>
-      <input type="text" placeholder="type here" ref={this.input} />
-      <button type="button" onClick={this.add}>Add!</button>
-      <hr />
-      { items.map((item, i) => <OTextInput
-        key={item}
-        label={`Item ${i + 1}`}
-        defaultValue={item}>
-        <button type="button" onClick={() => this.remove(i)}>Remove!</button>
-      </OTextInput>) }
-    </React.Fragment>;
-  }
-}
-
-const listStyle = {
-  border: '1px solid black',
-  display: 'inline-block',
-  verticalAlign: 'top',
-  width: '49%',
-};
-
 ReactDOM.render(<React.Fragment>
+  <h1 className="form--title">React and Web Components</h1>
+  <p>
+    This page contains some examples of using the o-input web component in React and the issues that arise. General problems are:
+  </p>
+  <ul>
+    <li>React is declarative, while Web Components are imperative.</li>
+    <li>React is not designed for two way data binding.</li>
+    <li>React properties can be any sort of data (arrays, objects, strings, functions, other React components), while web component attributes must be strings and slots must be DOM nodes.</li>
+    <li>React normalizes events to have a consistent interface across browsers and exposes them as callbacks. This convenience isn&apos;t possible for custom events.</li>
+    <li>Native inputs have a huge interface that both our wrapper and web component would need to duplicate.</li>
+  </ul>
+  <p>
+    Specific instances of these problems are demonstrated below.
+  </p>
+  <hr />
   <ControlledInput />
   <br />
   <Refs />
@@ -336,12 +296,4 @@ ReactDOM.render(<React.Fragment>
   <Events />
   <br />
   <Styles />
-  <br />
-  <div style={listStyle}>
-    <List />
-  </div>
-  <div style={listStyle}>
-    <OList />
-  </div>
-  <br />
 </React.Fragment>, document.getElementById("root"));
